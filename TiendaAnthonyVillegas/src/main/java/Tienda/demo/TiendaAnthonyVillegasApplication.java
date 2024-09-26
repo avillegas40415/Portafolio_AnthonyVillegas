c780ebccc165a9fa0853fc59b94e88844df94e05
avillegas40415 <avillegas40415@ufide.ac.cr>
2024-09-25 20:05:13 -0600

    Creación de Proyecto
    Se creo proyecto desde 0

---
+++ TiendaAnthonyVillegas/src/main/java/Tienda/demo/TiendaAnthonyVillegasApplication.java
@@ -0,0 +1,13 @@
+package Tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaAnthonyVillegasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaAnthonyVillegasApplication.class, args);
+	}
+
+}
